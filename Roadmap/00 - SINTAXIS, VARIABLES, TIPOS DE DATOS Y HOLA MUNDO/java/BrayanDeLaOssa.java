a346604d8b9aaf6f530d2184f5705d07cf2705b2
BrayanDeLaOssa <brayanfelipeffsksm@gmail.com>
2025-06-27 12:44:23 -0500

    Create BrayanDeLaOssa.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/BrayanDeLaOssa.java
@@ -0,0 +1,36 @@
+//https://www.java.com/es/
+package com.mycompany.retos_programacion;
+
+public class Retos_programacion {
+
+    public static void main(String[] args) {
+
+// -- esto es un comentario no se vera en consola te sirve para ayudarte a mantener tu codigo comentado 
+        //tipos de datos
+        int a = 12;
+        //datos enteros 
+        String String = "hola";
+        //datos tipo cadena o String
+        boolean respuesta = true;
+        //boolean true verdadero o false /falso 
+        double altura = 1.78;
+        // double tipo de dato decimal
+        final double pi = 3.14;
+        //final se usa para hacer un dato constante asi lo entiendo yo
+        char letra = 'B';
+        // un solo valor las variables se nombra el tipo , se le coloca un nombre y el valor que desea que tenga
+        System.out.println("Hola programando en java");
+        //esto es una imprecion por consola sirve para mostrar menajes y varibales en la consola
+        System.out.println(a);
+
+        System.out.println(String);
+
+        System.out.println(respuesta);
+
+        System.out.println(altura);
+
+        System.out.println(pi);
+
+        System.out.println(letra);
+    }
+}
